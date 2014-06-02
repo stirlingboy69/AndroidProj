10656c684ad57d85b4b782d1f20bde6925bb099e
stirlingboy69 <stirlingboy69@gmail.com>
2014-06-01 21:17:45 -0700

    Added start thread in on surfaceCreate

--- src/Android/Arduino/Bluetooth/ButtonSurfaceA.java
+++ src/Android/Arduino/Bluetooth/ButtonSurfaceA.java
@@ -8,6 +8,7 @@ import android.graphics.RectF;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.util.*;
+import android.os.SystemClock;
 
 public class ButtonSurfaceA extends SurfaceView implements Runnable, SurfaceHolder.Callback {
 
@@ -15,6 +16,8 @@ public class ButtonSurfaceA extends SurfaceView implements Runnable, SurfaceHold
 	private SurfaceHolder mSurfaceHolder;
 	private Thread mRunningThread;
 	private boolean mIsRunning=false;
+	private boolean mFlipColor=false;
+	private long mElapsedTime;
 	
 	public ButtonSurfaceA(Context context){
 		super(context);
@@ -38,13 +41,16 @@ public class ButtonSurfaceA extends SurfaceView implements Runnable, SurfaceHold
 		mSurfaceHolder = getHolder();
 		mSurfaceHolder.addCallback(this);
 		mRunningThread = new Thread(this);
-		mRunningThread.start();
+		mElapsedTime =  SystemClock.elapsedRealtime();
+		//mRunningThread.start();
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while(mIsRunning){
+			long time = SystemClock.elapsedRealtime();
+
 			mSurfaceHolder = getHolder();
 			if(!mSurfaceHolder.getSurface().isValid())
 				continue;
@@ -54,65 +60,72 @@ public class ButtonSurfaceA extends SurfaceView implements Runnable, SurfaceHold
 			Canvas canvas = mSurfaceHolder.lockCanvas();
 			
 			//ALL PAINT-JOB MAKE IN draw(canvas); method.
-			Draw(canvas);
+			Draw(canvas, time);
 			
 			// End of painting to canvas. system will paint with this canvas,to the surface.
 			mSurfaceHolder.unlockCanvasAndPost(canvas);
 		}
 	}
 	/**This method deals with paint-works. Also will paint something in background*/
-	private void Draw(Canvas canvas) {
+	private void Draw(Canvas canvas, long time) {
 		// paint a background color
+		boolean flipme=false;
+		if(time-mElapsedTime>=100)
+		{
+			flipme=true;
+			mElapsedTime = time;
+		}
 		canvas.drawColor(Color.BLACK);
-		
+
 		// paint a rectangular shape that fill the surface.
 		int border = 20;
 		RectF r = new RectF(border, border, canvas.getWidth()-20, canvas.getHeight()-20);
-		Paint paint = new Paint();		
-		paint.setARGB(200, 200, 15, 1); //paint color GRAY+SEMY TRANSPARENT 
+		Paint paint = new Paint();
+		if (mFlipColor)
+		{
+			paint.setARGB(200, 200, 15, 1); //paint color GRAY+SEMY TRANSPARENT
+			if(flipme)	mFlipColor=false;
+		}
+		else
+		{
+			paint.setARGB(200, 20, 150, 1); //paint color GRAY+SEMY TRANSPARENT
+			if(flipme) mFlipColor=true;
+		}
 		canvas.drawRect(r , paint );
 		
 		//canvas.drawCircle(canvas.getWidth()/4*3, canvas.getHeight()/2, radiusWhite, paint);
 	}
 
 	@Override
-	protected void onDraw(Canvas canvas) {
-		// TODO Auto-generated method stub
-		super.onDraw(canvas);
-		Log.i("onDraw","drawingxx");
-		canvas.drawColor(Color.WHITE);
-	}
-
-	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
 		// TODO Auto-generated method stub
-		Log.i("surfaceChanged","width="+Integer.toString(width)+",height="+Integer.toString(height) );
-		 Canvas c = getHolder().lockCanvas();
-			int border = 20;
-			RectF r = new RectF(border, border, c.getWidth()-20, c.getHeight()-20);
-			Paint paint = new Paint();		
-			paint.setARGB(200, 1, 135, 1); //paint color GRAY+SEMY TRANSPARENT 
-			c.drawRect(r , paint );
-		    getHolder().unlockCanvasAndPost(c);	 
+		//Log.i("surfaceChanged","width="+Integer.toString(width)+",height="+Integer.toString(height) );
+		mIsRunning = true;
+		mRunningThread.start();
 	}
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
-		 mIsRunning=true;
-		 Log.i("surfaceCreated","mIsRunning");
-	       //SurfaceHolder holder = getHolder();
-	        Canvas canvas = holder.lockCanvas();
-
-	        holder.unlockCanvasAndPost(canvas);
 
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
+		Log.i("surfaceDestroyed call","");
+		mIsRunning = false;
+		boolean retry = true;
 		
+		while (retry) {
+			try {
+				mRunningThread.join();
+				retry = false;
+			} 
+			catch (InterruptedException e) {
+			}
+		}
 	}
 	
 	
